1d80359be807b72de0886dcde886ca11117502e4
YChao <290388252@qq.com>
2020-04-29 17:44:13 +0800

    1

---
+++ week03/convertNumberToString.js
@@ -0,0 +1,29 @@
+function convertNumberToString(number, x) {
+	var integer = Math.floor(number);
+	var fraction = parseFloat((number - integer).toPrecision(12));
+	var string = '';
+
+	if(arguments.length < 2) {
+		x = 10;
+	}
+
+	if(typeof number === 'string') {
+		return number;
+	}
+	
+	if(number === 0) {
+		return '0';
+	}
+
+	while(integer > 0) {
+		string = String(integer % x) + string;
+		integer = Math.floor(integer / x);
+	}
+
+	while(fraction > 0) {
+		string = string + (fraction + '').slice(1);
+		fraction = 0;
+	}
+
+	return string;
+}
---
+++ week03/convertStringToNumber.js
@@ -0,0 +1,34 @@
+function convertStringToNumber(str, hex = 10) {
+  // 处理转换进制是否在2-36之间
+  if (typeof hex !== "number" || hex < 2 || hex > 36) {
+    return NaN;
+  }
+
+  let num = 0;
+  let i = 0;
+
+  // 这里要判断str亦即没有传入任何值时应返回0
+  if (str) {
+    const arr = String(str).split(""); // 防止要转换的值不是字符串
+    while (i < arr.length && arr[i] !== ".") {
+      const element = arr[i];
+      num = num * hex;
+      num += element.codePointAt(0) - "0".codePointAt(0);
+      i++;
+    }
+
+    if (arr[i] === ".") {
+      i++;
+    }
+
+    let decimal = 1;
+    while (i < arr.length) {
+      const element = arr[i];
+      decimal = decimal / hex;
+      num += (element.codePointAt(0) - "0".codePointAt(0)) * decimal;
+      i++;
+    }
+  }
+
+  return num;
+}
